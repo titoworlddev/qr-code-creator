5c8ab177fcb095c09bbafaffea4259a7ff75909e
titoworlddev <titocristian.dev@gmail.com>
2023-11-01 21:16:41 +0100

    feat: Created english traduction

--- src/languages/english.js
+++ src/languages/english.js
@@ -1,1 +1,12 @@
-export const english = {};
+export const english = {
+  header: {
+    h1: 'Create your QR code',
+    p: 'Enter a URL or text to generate the code'
+  },
+  form: {
+    submitButton: {
+      defaultText: 'Generate code',
+      generatingText: 'Generating code...'
+    }
+  }
+};
