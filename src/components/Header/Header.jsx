7ba647df0c5463082a226dfca8efcf76a705482f
titoworlddev <titocristian.dev@gmail.com>
2023-11-01 04:18:49 +0100

    feat: Extract Header in component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Crea tu código QR</h1>
+      <p>Ingresa una URL para generar un código QR</p>
+    </header>
+  );
+}
